454bcddab7c096e2e05bcba63ad2e4a620801591
Guilherme Men <62121362+GuilhermeNairne@users.noreply.github.com>
2024-04-11 21:16:57 -0300

    Add files via upload

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,24 @@
-import { Text } from "@chakra-ui/react";
-
+import { Box, Flex, Img, Link, Text } from "@chakra-ui/react";
 export function Home() {
     return (
-        <Text>Api Marvel</Text>
+        <Flex>
+            <Flex w={'full'} h={'130px'} bg={'black'} justifyContent={'center'} flexDir={'column'}>
+                <Box w={'full'} justifyContent={'center'} display={'flex'}>
+                    <Img src={'https://seeklogo.com/images/M/Marvel_Comics-logo-821795DF15-seeklogo.com.png'} h={'80px'}/>
+                </Box>
+                <Box w={'full'} h={'1px'} bg={'white'}/>
+                <Flex mt={'10px'} justifyContent={'center'} gap={15}>
+                    <Link>
+                    <Text color={'white'}>Personagens</Text>
+                    </Link>
+                    <Link>
+                    <Text color={'white'}>Histórias</Text>
+                    </Link>
+                    {/* <Link>
+                    <Text color={'white'}>Personagens</Text>
+                    </Link> */}
+                </Flex>
+            </Flex>
+        </Flex>
     )
 }
